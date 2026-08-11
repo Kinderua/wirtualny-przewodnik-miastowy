2b9cdca60b350fe3383a1c4bc527d9072cff7ad4
Kinderu <jahnanteksp13@gmail.com>
2026-08-11 22:54:06 +0200

    fgdsgdg

--- app/components/Alert.tsx
+++ app/components/Alert.tsx
@@ -1,12 +1,10 @@
-import React from 'react'
-
 const Alert = () => {
   return (
     <>
         <div className="alert alert-warning alert-dismissible fade show" role="alert">
             <p>A simple primary alert—check it out!</p>
             <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-        </div>
+        </div> 
     </>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,9 @@
 
 import Header from "./components/Header";
 import Button from "./components/Button";
-import Alert from "./components/Alert";
 import 'bootstrap/dist/css/bootstrap.css';
 
 const Home = () => {
-
-  const [isActive,setIsActive] = useEffect(false)
 
   return (<>
     <Header></Header>
