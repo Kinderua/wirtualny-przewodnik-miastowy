d89f458477cef2e9930a59571818763ee615cdd3
Kinderu <jahnanteksp13@gmail.com>
2026-08-11 18:18:32 +0200

    fsfsd

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,12 +15,8 @@ export default function RootLayout({
       lang="en"
       className="h-full antialiased"
     >
-      <head>
-        <link href="https://jsdelivr.net" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossOrigin="anonymous">
-        </link>
-      </head>
       <body className="min-h-full flex flex-col">{children}</body>
-      <script src="https://jsdelivr.net" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossOrigin="anonymous"></script>
+
     </html>
   );
 }
