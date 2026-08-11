72759adb225c9bc60aa45289a4d309b27e2cf862
Kinderu <jahnanteksp13@gmail.com>
2026-08-11 18:42:46 +0200

    fix 100%

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -4,7 +4,7 @@ interface Props {
     onClick : ()=>void;
 }
 
-const Button = ({color, children} : Props) => {
+const Button = ({color, children, onClick} : Props) => {
     return (
         <>
             <button type="button" className={"btn btn-" + color} onClick={onClick}>{children}</button>
