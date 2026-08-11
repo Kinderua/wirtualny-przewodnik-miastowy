8b59fe616bfab5fa81f9a964b789a77ec83a49ac
Kinderu <jahnanteksp13@gmail.com>
2026-08-11 18:11:02 +0200

    idk

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,12 +1,12 @@
 interface Props {
-    whatButton: string;
+    color: string;
     children: string;
 }
 
-const Button = ({whatButton, children} : Props) => {
+const Button = ({color, children} : Props) => {
     return (
         <>
-            <button type="button" className={"btn btn-" + whatButton}>{children}</button>
+            <button type="button" className={"btn btn-" + color}>{children}</button>
         </>
     )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,4 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,7 +13,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className="h-full antialiased"
     >
       <head>
         <link href="https://jsdelivr.net" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossOrigin="anonymous">
