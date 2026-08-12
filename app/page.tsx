f4d409d0d0a587f703b4e168cec1bcdb95600826
Kinderu <jahnanteksp13@gmail.com>
2026-08-12 10:16:35 +0200

    sdawads

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,12 @@
 "use client";
 
 import Header from "./components/Header";
-import Button from "./components/Button";
 import 'bootstrap/dist/css/bootstrap.css';
 
 const Home = () => { 
 
   return (<>
     <Header></Header>
-    <Button color="secondary">Button</Button>
   </>)
 }
 
