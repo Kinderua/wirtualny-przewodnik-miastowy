09c0a5a68a476255fadf86f29f6c303fbf6cd4f8
Kinderu <jahnanteksp13@gmail.com>
2026-08-17 11:58:44 +0200

    test 2

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,37 +1,18 @@
 const Header = () => {
   return (<>
-    <section className="bg-white lg:grid lg:h-screen lg:place-content-center dark:bg-gray-900">
-      <div className="mx-auto w-screen max-w-7xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8 lg:py-32">
-        <div className="mx-auto max-w-prose text-center">
-          <h1 className="text-4xl font-bold text-gray-900 sm:text-5xl dark:text-white">
-            Understand user flow and
-            <strong className="text-indigo-600"> increase </strong>
-            conversions
-          </h1>
-
-          <p className="mt-4 text-base text-pretty text-gray-700 sm:text-lg/relaxed dark:text-gray-200">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque, nisi. Natus, provident
-            accusamus impedit minima harum corporis iusto.
-          </p>
-
-          <div className="mt-4 flex justify-center gap-4 sm:mt-6">
-            <a
-              className="inline-block rounded border border-indigo-600 bg-indigo-600 px-5 py-3 font-medium text-white shadow-sm transition-colors hover:bg-indigo-700"
-              href="#"
-            >
-              Get Started
-            </a>
-
-            <a
-              className="inline-block rounded border border-gray-200 px-5 py-3 font-medium text-gray-700 shadow-sm transition-colors hover:bg-gray-50 hover:text-gray-900 dark:border-gray-700 dark:text-gray-200 dark:hover:bg-gray-800 dark:hover:text-white"
-              href="#"
-            >
-              Learn More
-            </a>
-          </div>
+  <header className="w-full">
+    <nav className="border-gray-200 bg-gray-900 py-2.5">
+      <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between px-4">
+        <a href="#" className="flex items-center"><span className="self-center text-xl font-semibold whitespace-nowrap text-white">Issue Tracker</span></a>
+        <div className="flex items-center lg:order-2">
+          <a className="rounded-lg border-2 border-white px-4 py-2 text-sm leading-[24px] font-medium text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 focus:outline-none sm:mr-2 lg:px-5 lg:py-2.5 dark:hover:bg-gray-700 dark:focus:ring-gray-800" href="/"
+            ><svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"></path></svg></a>
+          <a className="rounded-lg border-2 border-white px-4 py-2 text-sm leading-[24px] font-medium text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 focus:outline-none sm:mr-2 lg:px-5 lg:py-2.5 dark:hover:bg-gray-700 dark:focus:ring-gray-800" href="/guest">Log out</a>
         </div>
       </div>
-    </section>
+    </nav>
+  </header>
   </>)
 }
 
